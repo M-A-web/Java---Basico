7a572e01393168e8ebb599c643b276f4a7b4076b
M-A-web <matias_ayala87@outlook.com>
2023-01-16 15:56:29 -0300

    subiendo proyecto

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String nombre = "Matias";
+        int edad = 35;
+        long dni = 123456789;
+        double altura = 1.85d;
+        boolean casado = true;
+
+        System.out.println("nombre: "+nombre+'\n' +"edad: "+edad+" años"+'\n'+"dni: "+dni+'\n'+"altura: "+altura+'\n'+"estado civil: "+casado);
+    }
+}
